fc7acda0828139ed91f8d63654ce5eff3678dde6
dmuehlema <dmuehlema@student.ethz.ch>
2025-02-07 10:29:40 +0100

    update slurm

--- data/project_data_loader.py
+++ data/project_data_loader.py
@@ -14,8 +14,15 @@ from torch.utils.data import Dataset
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Custom imports
+import json
+import os
+from torchvision import transforms
+import json
+import os
+from torchvision.datasets import VisionDataset
+from torchvision.datasets.folder import default_loader
+from torchvision.datasets.utils import check_integrity, extract_archive
+from torchvision.datasets.utils import download_url, verify_str_arg
 
 
 ### AUTHORSHIP INFORMATION ###
@@ -253,3 +260,125 @@ class CIFAR100(Dataset):
         if self.transform:
             img = self.transform(img.float())
         return img, label 
+    
+
+
+class INat2017(VisionDataset):
+    """`iNaturalist 2017 <https://github.com/visipedia/inat_comp/blob/master/2017/README.md>`_ Dataset.
+
+        Args:
+            root (string): Root directory of the dataset.
+            split (string, optional): The dataset split, supports ``train``, or ``val``.
+            transform (callable, optional): A function/transform that  takes in an PIL image
+               and returns a transformed version. E.g, ``transforms.RandomCrop``
+            target_transform (callable, optional): A function/transform that takes in the
+               target and transforms it.
+            download (bool, optional): If true, downloads the dataset from the internet and
+               puts it in root directory. If dataset is already downloaded, it is not
+               downloaded again.
+    """
+    base_folder = 'train_val_images/'
+    file_list = {
+        'imgs': ('https://storage.googleapis.com/asia_inat_data/train_val/train_val_images.tar.gz',
+                 'train_val_images.tar.gz',
+                 '7c784ea5e424efaec655bd392f87301f'),
+        'annos': ('https://storage.googleapis.com/asia_inat_data/train_val/train_val2017.zip',
+                  'train_val2017.zip',
+                  '444c835f6459867ad69fcb36478786e7')
+    }
+
+    def __init__(self, root, split='train', transform=None, target_transform=None, download=False):
+        super(INat2017, self).__init__(root, transform=transform, target_transform=target_transform)
+        self.loader = default_loader
+        self.split = verify_str_arg(split, "split", ("train", "val",))
+
+        if self._check_exists():
+            print('Files already downloaded and verified.')
+        elif download:
+            if not (os.path.exists(os.path.join(self.root, self.file_list['imgs'][1]))
+                    and os.path.exists(os.path.join(self.root, self.file_list['annos'][1]))):
+                print('Downloading...')
+                self._download()
+            print('Extracting...')
+            extract_archive(os.path.join(self.root, self.file_list['imgs'][1]))
+            extract_archive(os.path.join(self.root, self.file_list['annos'][1]))
+        else:
+            raise RuntimeError(
+                'Dataset not found. You can use download=True to download it.')
+        anno_filename = split + '2017.json'
+        with open(os.path.join(self.root, anno_filename), 'r') as fp:
+            all_annos = json.load(fp)
+
+        self.annos = all_annos['annotations']
+        self.images = all_annos['images']
+
+    def __getitem__(self, index):
+        path = os.path.join(self.root, self.images[index]['file_name'])
+        target = self.annos[index]['category_id']
+        image =plt.imread(path)
+
+        # check if numpy array has dimension (x, y, 3)$
+        if len(image.shape) == 2:
+            # Convert grayscale to RGB by duplicating the channel
+            image = image[:, :, None]  # Add a new channel dimension
+            image = image.repeat(3, axis=2)  # Repeat the channel 3 times
+        elif image.shape[2] == 4:
+            image = image[:, :, :3]  
+
+        elif image.shape[2] != 3:
+            # return zero image
+            image = np.zeros((224, 224, 3))
+       
+        image = torch.tensor(image).permute(2, 0, 1)
+        #if self.split == 'train':
+        #    # AugMix: Perform data augmentation
+        #    image = transforms.AugMix()(image)
+        
+        #self.plot_image(image)
+        image = image.float()
+ 
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        
+        return image, target
+    
+    def plot_image(self, image):
+        """Utility function to plot an image."""
+        if torch.is_tensor(image):
+            # Convert CHW to HWC for plotting
+            image = image.permute(1, 2, 0).numpy()
+
+            # max and min pixel values
+            min_val = image.min()
+            max_val = image.max()
+            print(f'Min pixel value: {min_val}')
+            print(f'Max pixel value: {max_val}')
+
+        # denormalize
+        #image = (image) * 255
+        #min_val = image.min()
+        #max_val = image.max()
+        #print(f'Min pixel value: {min_val}')
+        # Convert to uint8
+        #image = image.astype(np.uint8)
+        if isinstance(image, transforms.ToTensor):
+            image = transforms.ToPILImage()(image)  # Convert back to PIL for display
+
+        plt.imshow(image)
+        plt.axis('off')
+        plt.show()
+
+    def __len__(self):
+        return len(self.images)
+
+    def _check_exists(self):
+        return os.path.exists(os.path.join(self.root, self.base_folder))
+
+    def _download(self):
+        for url, filename, md5 in self.file_list.values():
+            download_url(url, root=self.root, filename=filename)
+            if not check_integrity(os.path.join(self.root, filename), md5):
+                raise RuntimeError("File not found or corrupted.")
+            
